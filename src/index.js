37180ad3f2839c72e80e4eb944c70d9472e4be2c
jacobjolibois <jacobjolibois@hey.com>
2022-01-14 15:19:20 -0600

    attempted fix to test suite bug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import '@testing-library/dom';
 
 const { worker } = require('./mocks/browser');
 worker.start();
